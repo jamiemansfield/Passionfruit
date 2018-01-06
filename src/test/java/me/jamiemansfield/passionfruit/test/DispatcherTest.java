883f5b7aceb681bd8eac2b9c8d101f11924faa1d
Jamie Mansfield <dev@jamierocks.uk>
2018-01-06 15:02:20 +0000

    Modularise DispatcherTest

--- src/test/java/me/jamiemansfield/passionfruit/test/DispatcherTest.java
+++ src/test/java/me/jamiemansfield/passionfruit/test/DispatcherTest.java
@@ -2,9 +2,12 @@ package me.jamiemansfield.passionfruit.test;
 
 import static org.junit.Assert.assertEquals;
 
+import com.google.common.collect.ImmutableMap;
 import me.jamiemansfield.passionfruit.CommandDispatcher;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Map;
 
 public final class DispatcherTest {
 
@@ -12,30 +15,52 @@ public final class DispatcherTest {
         return new CommandDispatcher<>();
     }
 
+    private static void registerSimpleCommand(final CommandDispatcher<TestCommandCaller> dispatcher, final String command, final String result) {
+        dispatcher.register(command, (caller, args) -> caller.message(result));
+    }
+
+    private static final Map<String, String> COMMAND_TESTS = ImmutableMap.<String, String>builder()
+            .put("test", "test")
+            .build();
+
+    private static final Map<String, Map<String, String>> SUBCOMMAND_TESTS = ImmutableMap.<String, Map<String, String>>builder()
+            .put("example", ImmutableMap.<String, String>builder()
+                    .put("test", "test")
+                    .put("example", "example")
+                    .build())
+            .build();
+
     private static CommandDispatcher<TestCommandCaller> dispatcher;
     private static TestCommandCaller caller;
 
     @BeforeClass
     public static void initialise() {
-        dispatcher = createDispatcher()
-            .register("test", (caller, args) -> caller.message("test"))
-            .register("example", createDispatcher()
-                    .register("test", (caller, args) -> caller.message("test"))
-                    .register("example", (caller, args) -> caller.message("example"))
-            );
+        dispatcher = createDispatcher();
+
+        COMMAND_TESTS.forEach((command, result) -> registerSimpleCommand(dispatcher, command, result));
+        SUBCOMMAND_TESTS.forEach((rootCommand, childCommands) -> {
+            final CommandDispatcher<TestCommandCaller> rootDispatcher = createDispatcher();
+            childCommands.forEach((command, result) -> registerSimpleCommand(rootDispatcher, command, result));
+            dispatcher.register(rootCommand, rootDispatcher);
+        });
+
         caller = new TestCommandCaller();
     }
 
     @Test
     public void commandTest() {
-        dispatcher.execute(caller, "test");
-        assertEquals("test", caller.getMessage());
+        COMMAND_TESTS.forEach((command, expectedResult) -> {
+            dispatcher.execute(caller, command);
+            assertEquals(expectedResult, caller.getMessage());
+        });
     }
 
     @Test
     public void subcommandTest() {
-        dispatcher.execute(caller, "example example");
-        assertEquals("example", caller.getMessage());
+        SUBCOMMAND_TESTS.forEach((rootCommand, childCommands) -> childCommands.forEach((command, expectedResult) -> {
+            dispatcher.execute(caller, rootCommand + " " + command);
+            assertEquals(expectedResult, caller.getMessage());
+        }));
     }
 
 }
